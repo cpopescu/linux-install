e1601ba7080c3e5e9658c09c789a8988aaef7027
Catalin Popescu <cpopescu@gmail.com>
2024-09-16 11:02:23 -0700

    Fixed the log file

--- build/run_server.sh
+++ build/run_server.sh
@@ -3,11 +3,11 @@
 mkdir -p ~/logs
 
 function start_server {
-    echo "Starting worker"
+    echo "Starting server"
     cd ~/src/bazel-buildfarm
     bazel run //src/main/java/build/buildfarm:buildfarm-server \
           -- --jvm_flag=-Djava.util.logging.config.file=${PWD}/examples/logging.properties \
-          ${PWD}/examples/config.minimal.yml > ~/logs/worker.log 2>&1 &
+          ${PWD}/examples/config.minimal.yml > ~/logs/server.log 2>&1 &
 }
 
 curl -s http://localhost:9090/health > /dev/null || start_server
